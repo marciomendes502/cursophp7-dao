1d19c81ae2433549cf133002c60ed93aa191118c
marciomendes502 <marciogleisson@gmail.com>
2020-06-05 12:30:19 -0300

    criamos a função login e também search e outras

--- index.php
+++ index.php
@@ -2,13 +2,27 @@
 
 require_once("config.php");
 
+// carrega somente um usuario
+//$usuario = new Usuario();
+//$usuario->loadById(3);
+//echo $usuario;
 
+// carrega uma lista de usuários
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+// carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("lio");
+//echo json_encode($search);
+
+//carrega um usuario usando o login e a senha
 $usuario = new Usuario();
-
-$usuario->loadById(3);
+$usuario->login("getulio@yahoo.com.br","4444");
 
 echo $usuario;
-
+//primeiro exemplo
 /*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
